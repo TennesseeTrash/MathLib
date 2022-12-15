eddde10bd104d141fb4180a5daa1dd781300465d
Viktor Soukup <vitosoukup@gmail.com>
2022-12-15 21:51:59 +0100

    Improve layout, add harness for tests

---
+++ Tests/TestVectors.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_test_macros.hpp>
+#include <Math/Vector.hpp>
+
+TEST_CASE("Stub test case", "[Math][Vector]")
+{
+    SECTION("Stub section")
+    {
+        REQUIRE(true);
+    }
+}

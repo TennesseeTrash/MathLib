c7c3b6d42a67423ce901798be0afbf4051a69b6c
Viktor Soukup <vitosoukup@gmail.com>
2023-09-14 22:36:49 +0200

    Add types (forgot to add to previous commit)

---
+++ MathLib/Include/Math/Common/Types.hpp
@@ -0,0 +1,30 @@
+#ifndef MATHLIB_COMMON_TYPES_HPP
+#define MATHLIB_COMMON_TYPES_HPP
+
+#include <cstdint>
+#include <cstddef>
+#include <limits>
+
+namespace Math
+{
+    static_assert(std::numeric_limits<float>::is_iec559);
+    static_assert(std::numeric_limits<double>::is_iec559);
+
+    static_assert(sizeof(float) == 4);
+    static_assert(sizeof(double) == 8);
+
+    using i8  = int8_t;
+    using i16 = int16_t;
+    using i32 = int32_t;
+    using i64 = int64_t;
+
+    using u8  = uint8_t;
+    using u16 = uint16_t;
+    using u32 = uint32_t;
+    using u64 = uint64_t;
+
+    using f32 = float;
+    using f64 = double;
+}
+
+#endif //MATHLIB_COMMON_TYPES_HPP

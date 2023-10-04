5217713336901a3bd67b71ce6230c3545134c5d3
Viktor Soukup <vitosoukup@gmail.com>
2023-10-04 13:38:35 +0200

    Add std:: prefix to the type aliases
    (because we're using cstdint instead of stdint.h)

--- MathLib/Include/Math/Common/Types.hpp
+++ MathLib/Include/Math/Common/Types.hpp
@@ -9,21 +9,21 @@ namespace Math
 {
     using SizeType = std::size_t;
 
+    using i8  = std::int8_t;
+    using i16 = std::int16_t;
+    using i32 = std::int32_t;
+    using i64 = std::int64_t;
+
+    using u8  = std::uint8_t;
+    using u16 = std::uint16_t;
+    using u32 = std::uint32_t;
+    using u64 = std::uint64_t;
+
     static_assert(std::numeric_limits<float>::is_iec559);
     static_assert(std::numeric_limits<double>::is_iec559);
 
     static_assert(sizeof(float) == 4);
     static_assert(sizeof(double) == 8);
-
-    using i8  = int8_t;
-    using i16 = int16_t;
-    using i32 = int32_t;
-    using i64 = int64_t;
-
-    using u8  = uint8_t;
-    using u16 = uint16_t;
-    using u32 = uint32_t;
-    using u64 = uint64_t;
 
     using f32 = float;
     using f64 = double;

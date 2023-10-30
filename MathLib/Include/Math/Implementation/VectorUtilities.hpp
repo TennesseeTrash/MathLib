c4650ccc519d4633d666b99424b134b4b511ed4d
Viktor Soukup <vitosoukup@gmail.com>
2023-10-30 23:38:48 +0100

    Add some useful vector utilities

--- MathLib/Include/Math/Implementation/VectorUtilities.hpp
+++ MathLib/Include/Math/Implementation/VectorUtilities.hpp
@@ -34,9 +34,16 @@ namespace Math
 
     template <ConceptVector2 Vec>
     [[nodiscard]] constexpr
-    typename Vec::ScalarType PerpProduct(const Vec& u, const Vec& v) noexcept
+    typename Vec::ScalarType Area(const Vec& u, const Vec& v) noexcept
     {
         return u.x * v.y - u.y * v.x;
+    }
+
+    template <ConceptVector2 Vec>
+    [[nodiscard]] constexpr
+    typename Vec::ScalarType TriangleArea(const Vec& u, const Vec& v) noexcept
+    {
+        return Area(u, v) / 2;
     }
 
     template <Orientation Hand = Orientation::Right, ConceptVector3 Vec>
@@ -57,6 +64,66 @@ namespace Math
             w.z = u.y * v.x - u.x * v.y;
         }
         return w;
+    }
+
+    template <ConceptVector3 Vec>
+    [[nodiscard]] constexpr
+    typename Vec::ScalarType Area(const Vec& u, const Vec& v) noexcept
+    {
+        return Cross(u, v).Length();
+    }
+
+    template <ConceptVector3 Vec>
+    [[nodiscard]] constexpr
+    typename Vec::ScalarType TriangleArea(const Vec& u, const Vec& v) noexcept
+    {
+        return Area(u, v) / 2;
+    }
+
+    template <ConceptVector3 Vec>
+    [[nodiscard]] constexpr
+    typename Vec::ScalarType Volume(const Vec& u, const Vec& v, const Vec& w) noexcept
+    {
+        return Dot(u, Cross(v, w));
+    }
+
+    template <ConceptVector3 Vec>
+    [[nodiscard]] constexpr
+    typename Vec::ScalarType TetrahedronVolume(const Vec& u, const Vec& v, const Vec& w) noexcept
+    {
+        return Volume(u, v, w) / 6;
+    }
+
+    template <ConceptVector Vec>
+    [[nodiscard]] constexpr
+    Vec Reflect(const Vec& u, const Vec& n) noexcept
+    {
+        Vec unitN = Normalize(n);
+        return u - 2 * Dot(u, unitN) * unitN;
+    }
+
+    template <ConceptVector3 Vec>
+    [[nodiscard]] constexpr
+    Vec ReflectLocalZ(const Vec& u) noexcept
+    {
+        return Vec(-u.x, -u.y, u.z);
+    }
+
+    template <ConceptVector3 Vec>
+    [[nodiscard]] constexpr
+    Vec Refract(const Vec& u, const Vec& n, typename Vec::ScalarType eta) noexcept
+    {
+        using Scalar = typename Vec::ScalarType;
+
+        Scalar cosTheta = Dot(n, u);
+        Scalar sinThetaSquared = 1 - Squared(cosTheta);
+        Scalar sinPhiSquared = Squared(eta) * sinThetaSquared;
+        Scalar cosPhiSquared = 1 - sinPhiSquared;
+        if (cosPhiSquared < Scalar(0))
+        {
+            return Vec(0);
+        }
+        return eta * u - (eta * cosTheta + Sqrt(cosPhiSquared)) * n;
     }
 }
 

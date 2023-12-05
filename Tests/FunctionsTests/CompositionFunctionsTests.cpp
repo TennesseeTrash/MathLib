509ba614728ec8d572596dc71831c387a5cc3b5f
Viktor Soukup <vitosoukup@gmail.com>
2023-12-05 23:20:31 +0100

    Add basic tests for Minus and Subtract composite functions

--- Tests/FunctionsTests/CompositionFunctionsTests.cpp
+++ Tests/FunctionsTests/CompositionFunctionsTests.cpp
@@ -6,6 +6,42 @@ using f32 = Math::f32;
 
 // TODO(3011): Add tests for Minus, Subtract, Multiply, Divide, Compose and PowerCompose
 
+TEST_CASE("Test Minus composite function")
+{
+    using Linear = MF::Linear<f32, 2.0f, 1.0f>;
+    using Minus = MF::Minus<Linear>;
+
+    SECTION("Minus value")
+    {
+        constexpr Minus minus;
+        REQUIRE(Math::Equal(minus(0.0f), -1.0f));
+        REQUIRE(Math::Equal(minus(1.0f), -3.0f));
+        REQUIRE(Math::Equal(minus(2.0f), -5.0f));
+        REQUIRE(Math::Equal(minus(3.0f), -7.0f));
+        REQUIRE(Math::Equal(minus(4.0f), -9.0f));
+    }
+
+    SECTION("Minus derivative value")
+    {
+        constexpr MF::Derivative<Minus> derivative;
+        REQUIRE(Math::Equal(derivative(0.0f), -2.0f));
+        REQUIRE(Math::Equal(derivative(1.0f), -2.0f));
+        REQUIRE(Math::Equal(derivative(2.0f), -2.0f));
+        REQUIRE(Math::Equal(derivative(3.0f), -2.0f));
+        REQUIRE(Math::Equal(derivative(4.0f), -2.0f));
+    }
+
+    SECTION("Minus 2nd order derivative value")
+    {
+        constexpr MF::Derivative<Minus, 2> derivative;
+        REQUIRE(Math::Equal(derivative(0.0f), 0.0f));
+        REQUIRE(Math::Equal(derivative(1.0f), 0.0f));
+        REQUIRE(Math::Equal(derivative(2.0f), 0.0f));
+        REQUIRE(Math::Equal(derivative(3.0f), 0.0f));
+        REQUIRE(Math::Equal(derivative(4.0f), 0.0f));
+    }
+}
+
 TEST_CASE("Test Add composite function")
 {
     using Constant = MF::Constant<f32, 4.0f>;
@@ -41,5 +77,42 @@ TEST_CASE("Test Add composite function")
         REQUIRE(Math::Equal(derivative(2.0f), 2.0f));
         REQUIRE(Math::Equal(derivative(3.0f), 2.0f));
         REQUIRE(Math::Equal(derivative(4.0f), 2.0f));
+    }
+}
+
+TEST_CASE("Test Subtract composite function")
+{
+    using Quadratic = MF::Quadratic<f32, 3.0f, 2.0f, 1.0f>;
+    using Linear = MF::Linear<f32, 2.0f, 1.0f>;
+    using Subtract = MF::Subtract<Quadratic, Linear>;
+
+    SECTION("Subtract value")
+    {
+        constexpr Subtract difference;
+        REQUIRE(Math::Equal(difference(0.0f), 0.0f));
+        REQUIRE(Math::Equal(difference(1.0f), 3.0f));
+        REQUIRE(Math::Equal(difference(2.0f), 12.0f));
+        REQUIRE(Math::Equal(difference(3.0f), 27.0f));
+        REQUIRE(Math::Equal(difference(4.0f), 48.0f));
+    }
+
+    SECTION("Subtract derivative value")
+    {
+        constexpr MF::Derivative<Subtract> derivative;
+        REQUIRE(Math::Equal(derivative(0.0f), 0.0f));
+        REQUIRE(Math::Equal(derivative(1.0f), 6.0f));
+        REQUIRE(Math::Equal(derivative(2.0f), 12.0f));
+        REQUIRE(Math::Equal(derivative(3.0f), 18.0f));
+        REQUIRE(Math::Equal(derivative(4.0f), 24.0f));
+    }
+
+    SECTION("Subtract 2nd order derivative value")
+    {
+        constexpr MF::Derivative<Subtract, 2> derivative;
+        REQUIRE(Math::Equal(derivative(0.0f), 6.0f));
+        REQUIRE(Math::Equal(derivative(1.0f), 6.0f));
+        REQUIRE(Math::Equal(derivative(2.0f), 6.0f));
+        REQUIRE(Math::Equal(derivative(3.0f), 6.0f));
+        REQUIRE(Math::Equal(derivative(4.0f), 6.0f));
     }
 }

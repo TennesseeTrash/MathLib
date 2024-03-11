2fe63c323b7049c813f2b4e5cdb60f2c5cb33b45
Viktor Soukup <61194765+3O11@users.noreply.github.com>
2024-03-11 06:40:26 +0100

    Add BoundingCircle for 2D shapes (with a placeholder for Quadrilaterals)

--- MathLib/Include/Math/Implementation/Geometry/2D/Bounds.hpp
+++ MathLib/Include/Math/Implementation/Geometry/2D/Bounds.hpp
@@ -101,6 +101,91 @@ namespace Math::Geometry2D
 
         return Rectangle<T>(ellipse.Center - centerOffset, ellipse.Center + centerOffset);
     }
+
+
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    Circle<T> BoundingCircle(const Point<T>& point) noexcept
+    {
+        return Circle<T>(point, T(0));
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    Circle<T> BoundingCircle(const Line<T>& line) noexcept
+    {
+        Point<T> center = (line.Start + line.End) / T(2);
+        T        radius = (line.Start - center).Length();
+        return Circle<T>(center, radius);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    Circle<T> BoundingCircle(const Circle<T>& circle) noexcept
+    {
+        return Circle<T>(circle.Center, circle.Radius);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    Circle<T> BoundingCircle(const Triangle<T>& triangle) noexcept
+    {
+        Array<T, 3> squaredLengths(
+            Vector2T<T>(triangle.Vertices[0]).LenSqr(),
+            Vector2T<T>(triangle.Vertices[1]).LenSqr(),
+            Vector2T<T>(triangle.Vertices[2]).LenSqr()
+        );
+
+        T d = (triangle.Vertices[0].x * (triangle.Vertices[1].y - triangle.Vertices[2].y) +
+               triangle.Vertices[1].x * (triangle.Vertices[2].y - triangle.Vertices[0].y) +
+               triangle.Vertices[2].x * (triangle.Vertices[0].y - triangle.Vertices[1].y));
+
+        Point<T> center(
+            (squaredLengths[0] * (triangle.Vertices[1].y - triangle.Vertices[2].y) +
+             squaredLengths[1] * (triangle.Vertices[2].y - triangle.Vertices[0].y) +
+             squaredLengths[2] * (triangle.Vertices[0].y - triangle.Vertices[1].y)) / d,
+            (squaredLengths[0] * (triangle.Vertices[2].x - triangle.Vertices[1].x) +
+             squaredLengths[1] * (triangle.Vertices[0].x - triangle.Vertices[2].x) +
+             squaredLengths[2] * (triangle.Vertices[1].x - triangle.Vertices[0].x)) / d
+        );
+
+        T cosAngle = Dot(triangle.Vertices[1] - triangle.Vertices[0], triangle.Vertices[2] - triangle.Vertices[0]);
+        T sinAngle = Sqrt(1 - Squared(cosAngle));
+        T radius = (triangle.Vertices[1] - triangle.Vertices[2]).Length() / (2 * sinAngle);
+
+        return Circle<T>(center, radius);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    Circle<T> BoundingCircle(const Rectangle<T>& rectangle) noexcept
+    {
+        Point<T> center = Point<T>((Vector2T<T>(rectangle.Min) + Vector2T<T>(rectangle.Max)) / T(2));
+        T        radius = (center - rectangle.Min).Length();
+        return Circle<T>(center, radius);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    Circle<T> BoundingCircle(const Quadrilateral<T>& quadrilateral) noexcept
+    {
+        // TODO(3011): Actually implement properly, this will never
+        // be a tight bounding circle for any non-rectangular shape.
+        // Problems to solve:
+        //   - It would be nice to have this work for a general polygon.
+        //   - Using a proper algorithm to find the minimum bounding circle.
+        //     (Ideally with a convex hull.)
+        //       - For that, the constraint of not using any dynamic allocations must be solved.
+        return BoundingCircle(BoundingRectangle(quadrilateral));
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    Circle<T> BoundingCircle(const Ellipse<T>& ellipse) noexcept
+    {
+        return Circle<T>(ellipse.Center, Max(ellipse.Radii));
+    }
 }
 
 #endif //MATHLIB_IMPLEMENTATION_GEOEMTRY_2D_BOUNDING_RECTANGLE_HPP

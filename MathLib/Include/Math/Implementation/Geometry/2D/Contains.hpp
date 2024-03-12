0c2c7fd594b24554b774c1492225f932d9a48dba
Viktor Soukup <61194765+3O11@users.noreply.github.com>
2024-03-12 22:32:53 +0100

    Add (untested) Contains implementations

--- MathLib/Include/Math/Implementation/Geometry/2D/Contains.hpp
+++ MathLib/Include/Math/Implementation/Geometry/2D/Contains.hpp
@@ -4,14 +4,84 @@
 #include "Shapes.hpp"
 #include "../../../Transform.hpp"
 
+// Note(3011): I'm not sure if I like this sort of interface,
+// these functions might make more sense as members.
+
 namespace Math::Geometry2D
 {
+    //////////////////////////////////////////////////////////////////////////
+    // Point Contains functions
+    //////////////////////////////////////////////////////////////////////////
+
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr
     bool Contains(const Point<T>& point, const Point<T>& other) noexcept
     {
-        return Equal(point, other);
+        return Equal(point, other, Constant::GeometryEpsilon<T>);
     }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Point<T>& point, const Line<T>& line) noexcept
+    {
+        return Equal(point, line.Start, Constant::GeometryEpsilon<T>)
+            && Equal(point, line.End, Constant::GeometryEpsilon<T>);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Point<T>& point, const Circle<T>& circle) noexcept
+    {
+        return Equal(point, circle.Center, Constant::GeometryEpsilon<T>)
+            && Equal(circle.Radius, T(0), Constant::GeometryEpsilon<T>);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Point<T>& point, const Triangle<T>& triangle) noexcept
+    {
+        return Equal(point, triangle.Vertices[0], Constant::GeometryEpsilon<T>)
+            && Equal(point, triangle.Vertices[1], Constant::GeometryEpsilon<T>)
+            && Equal(point, triangle.Vertices[0], Constant::GeometryEpsilon<T>);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Point<T>& point, const Rectangle<T>& rectangle) noexcept
+    {
+        return Equal(point, rectangle.Min, Constant::GeometryEpsilon<T>)
+            && Equal(point, rectangle.Max, Constant::GeometryEpsilon<T>);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Point<T>& point, const Quadrilateral<T>& quadrilateral) noexcept
+    {
+        const SizeType vertexCount = quadrilateral.Vertices.Size;
+        for (SizeType i = 0; i < vertexCount; ++i)
+        {
+            if (!Equal(point, quadrilateral.Vertices[i], Constant::GeometryEpsilon<T>))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Point<T>& point, const Ellipse<T>& ellipse) noexcept
+    {
+        return Equal(point, ellipse.Center, Constant::GeometryEpsilon<T>)
+            && Equal(point, ellipse.Radii.x, Constant::GeometryEpsilon<T>)
+            && Equal(point, ellipse.Radii.y, Constant::GeometryEpsilon<T>);
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Line Contains functions
+    //////////////////////////////////////////////////////////////////////////
 
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr
@@ -21,12 +91,79 @@ namespace Math::Geometry2D
         return Equal(t.x, t.y) && t.x >= 0 && t.x <= 1;
     }
 
+    // TODO(3011): Would the rest of these make sense?
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Circle Contains functions
+    //////////////////////////////////////////////////////////////////////////
+
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr
     bool Contains(const Circle<T>& circle, const Point<T>& point) noexcept
     {
         return (point - circle.Center).Length() <= circle.Radius;
     }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Circle<T>& circle, const Line<T>& line) noexcept
+    {
+        return Contains(circle, line.Start) && Contains(circle, line.End);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Circle<T>& circle, const Circle<T>& other) noexcept
+    {
+        return (circle.Center - other.Center).Length() <= Abs(circle.Radius - other.Radius);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Circle<T>& circle, const Triangle<T>& triangle) noexcept
+    {
+        return Contains(circle, triangle.Vertices[0])
+            && Contains(circle, triangle.Vertices[1])
+            && Contains(circle, triangle.Vertices[2]);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Circle<T>& circle, const Rectangle<T>& rectangle) noexcept
+    {
+        return Contains(circle, rectangle.Min) && Contains(circle, rectangle.Max);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Circle<T>& circle, const Quadrilateral<T>& quadrilateral) noexcept
+    {
+        const SizeType vertexCount = quadrilateral.Vertices.Size;
+        for (SizeType i = 0; i < vertexCount; ++i)
+        {
+            if (!Contains(circle, quadrilateral.Vertices[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Circle<T>& circle, const Ellipse<T>& ellipse) noexcept
+    {
+        // TODO(3011): This is not going to be completely straight forward.
+        return false;
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Triangle Contains functions
+    //////////////////////////////////////////////////////////////////////////
 
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr
@@ -51,11 +188,138 @@ namespace Math::Geometry2D
 
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr
+    bool Contains(const Triangle<T>& triangle, const Line<T>& line) noexcept
+    {
+        return Contains(triangle, line.Start) && Contains(triangle, line.End);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Triangle<T>& triangle, const Circle<T>& circle) noexcept
+    {
+        // TODO(3011): Implement
+        return false;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Triangle<T>& triangle, const Triangle<T>& other) noexcept
+    {
+        return Contains(triangle, other.Vertices[0])
+            && Contains(triangle, other.Vertices[1])
+            && Contains(triangle, other.Vertices[2]);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Triangle<T>& triangle, const Rectangle<T>& rectangle) noexcept
+    {
+        return Contains(triangle, rectangle.Min)
+            && Contains(triangle, rectangle.Max)
+            && Contains(triangle, Point<T>(rectangle.Min.x, rectangle.Max.y))
+            && Contains(triangle, Point<T>(rectangle.Max.x, rectangle.Min.y));
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Triangle<T>& triangle, const Quadrilateral<T>& quadrilateral) noexcept
+    {
+        const SizeType vertexCount = quadrilateral.Vertices.Size;
+        for (SizeType i = 0; i < vertexCount; ++i)
+        {
+            if (!Contains(triangle, quadrilateral.Vertices[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Triangle<T>& triangle, const Ellipse<T>& ellipse) noexcept
+    {
+        // TODO(3011): Implement
+        return false;
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Rectangle Contains functions
+    //////////////////////////////////////////////////////////////////////////
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
     bool Contains(const Rectangle<T>& rectangle, const Point<T>& point) noexcept
     {
         return point.x >= rectangle.Min.x && point.x <= rectangle.Max.x &&
                point.y >= rectangle.Min.y && point.y <= rectangle.Max.y;
     }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Rectangle<T>& rectangle, const Line<T>& line) noexcept
+    {
+        return Contains(rectangle, line.Start) && Contains(rectangle, line.End);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Rectangle<T>& rectangle, const Circle<T>& circle) noexcept
+    {
+        Vector2T<T> min = circle.Center - rectangle.Min;
+        Vector2T<T> max = rectangle.Max - circle.Center;
+        return (min.x >= circle.Radius) && (min.y >= circle.Radius) &&
+               (max.x >= circle.Radius) && (max.y >= circle.Radius);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Rectangle<T>& rectangle, const Triangle<T>& triangle) noexcept
+    {
+        return Contains(rectangle, triangle.Vertices[0])
+            && Contains(rectangle, triangle.Vertices[1])
+            && Contains(rectangle, triangle.Vertices[2]);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Rectangle<T>& rectangle, const Rectangle<T>& other) noexcept
+    {
+        return rectangle.Min.x <= other.Min.x && rectangle.Min.y <= other.Min.y &&
+               rectangle.Max.x >= other.Max.x && rectangle.Max.y >= other.Max.y;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Rectangle<T>& rectangle, const Quadrilateral<T>& quadrilateral) noexcept
+    {
+        const SizeType vertexCount = quadrilateral.Vertices.Size;
+        for (SizeType i = 0; i < vertexCount; ++i)
+        {
+            if (!Contains(rectangle, quadrilateral.Vertices[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Rectangle<T>& rectangle, const Ellipse<T>& ellipse) noexcept
+    {
+        // TODO(3011): This is not going to be completely straight forward, but
+        // an easy way to solve this would be to make a bounding rectangle and check that.
+        return false;
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Quadrilateral Contains functions
+    //////////////////////////////////////////////////////////////////////////
 
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr
@@ -87,6 +351,15 @@ namespace Math::Geometry2D
         return windings != 0;
     }
 
+    // TODO(3011): These are going to be a major pain to implement, because we're not enforcing
+    // convexity and we want this to work for Polygons in general.
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Ellipse Contains functions
+    //////////////////////////////////////////////////////////////////////////
+
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr
     bool Contains(const Ellipse<T>& ellipse, Point<T> point) noexcept
@@ -94,6 +367,70 @@ namespace Math::Geometry2D
         point = Rotate(-ellipse.Angle) * Point<T>(point - ellipse.Center);
         return (Squared(point.x) / Squared(ellipse.Radii.x)) + (Squared(point.y) / Squared(ellipse.Radii.y)) <= 1;
     }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Ellipse<T>& ellipse, const Line<T>& line) noexcept
+    {
+        return Contains(ellipse, line.Start) && Contains(ellipse, line.End);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Ellipse<T>& ellipse, const Circle<T>& circle) noexcept
+    {
+        // TODO(3011): Implement
+        return false;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Ellipse<T>& ellipse, const Triangle<T>& triangle) noexcept
+    {
+        return Contains(ellipse, triangle.Vertices[0])
+            && Contains(ellipse, triangle.Vertices[1])
+            && Contains(ellipse, triangle.Vertices[2]);
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Ellipse<T>& ellipse, const Rectangle<T>& rectangle) noexcept
+    {
+        return Contains(ellipse, rectangle.Min)
+            && Contains(ellipse, rectangle.Max)
+            && Contains(ellipse, Point<T>(rectangle.Min.x, rectangle.Max.y))
+            && Contains(ellipse, Point<T>(rectangle.Max.x, rectangle.Min.y));
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Ellipse<T>& ellipse, const Quadrilateral<T>& quadrilateral) noexcept
+    {
+        const SizeType vertexCount = quadrilateral.Vertices.Size;
+        for (SizeType i = 0; i < vertexCount; ++i)
+        {
+            if (!Contains(ellipse, quadrilateral.Vertices[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <Concept::StrongFloatType T>
+    [[nodiscard]] constexpr
+    bool Contains(const Ellipse<T>& ellipse, const Ellipse<T>& other) noexcept
+    {
+        // TODO(3011): Implement
+        return false;
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Ray Contains functions
+    //////////////////////////////////////////////////////////////////////////
+    // I'm not sure if any other Contains functions would make sense for a Ray.
 
     template <Concept::StrongFloatType T>
     [[nodiscard]] constexpr

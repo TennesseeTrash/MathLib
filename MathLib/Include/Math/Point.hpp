cc80c20ca08d14f0e75577db03564b9fe767e3de
Viktor Soukup <61194765+3O11@users.noreply.github.com>
2024-05-14 22:24:17 +0200

    Add a few more type aliases

--- MathLib/Include/Math/Point.hpp
+++ MathLib/Include/Math/Point.hpp
@@ -10,6 +10,12 @@ namespace Math
     // Useful type aliases
     //////////////////////////////////////////////////////////////////////////
 
+    using Point2i = Point2T<i32>;
+    using Point3i = Point3T<i32>;
+
+    using Point2sz = Point2T<SizeType>;
+    using Point3sz = Point3T<SizeType>;
+
     using Point2f = Point2T<f32>;
     using Point3f = Point3T<f32>;
 
@@ -19,6 +25,12 @@ namespace Math
     //////////////////////////////////////////////////////////////////////////
     // Enforce concepts on provided types
     //////////////////////////////////////////////////////////////////////////
+
+    static_assert(Concept::Point2<Point2i>);
+    static_assert(Concept::Point3<Point3i>);
+
+    static_assert(Concept::Point2<Point2sz>);
+    static_assert(Concept::Point3<Point3sz>);
 
     static_assert(Concept::Point2<Point2f>);
     static_assert(Concept::Point3<Point3f>);
